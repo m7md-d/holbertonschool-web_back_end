90b128d55054009720059a714ec4673b9af74a6a
m7md-d <236364775+m7md-d@users.noreply.github.com>
2026-04-12 14:14:28 +0300

    doc to 0-add.py

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,3 +1,12 @@
 #!/usr/bin/env python3
+"""
+0. Basic annotations - add
+"""
+
+
 def add(a: float, b: float) -> float:
+    """
+    function add that takes a float a and a float b as arguments
+    and returns their sum as a float.
+    """
     return a + b
