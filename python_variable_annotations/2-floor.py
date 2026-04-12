8c22eebf9ee140bdd4d795ba9cbb6d01135085e9
m7md-d <236364775+m7md-d@users.noreply.github.com>
2026-04-12 14:24:38 +0300

    solve task 2

--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -1,1 +1,12 @@
 #!/usr/bin/env python3
+"""
+2. Basic annotations - floor
+"""
+
+
+def floor(n: float) -> int:
+    """
+    function floor which takes a float n as argument
+    and returns the floor of the float.
+    """
+    return int(n)
