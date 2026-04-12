8e835dddda18789021f6728cafb575dae61b0664
m7md-d <236364775+m7md-d@users.noreply.github.com>
2026-04-12 19:19:19 +0300

    solve task 8

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -1,1 +1,15 @@
 #!/usr/bin/env python3
+"""
+8. Complex types - functions
+"""
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """
+    function make_multiplier that takes a float multiplier as argument
+    and returns a function that multiplies a float by multiplier.
+    """
+    def mul(n: float):
+        return n * multiplier
+    return mul
