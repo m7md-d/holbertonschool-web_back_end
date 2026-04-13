251c197abbc9122c0a565ec795665307915a2c04
m7md-d <236364775+m7md-d@users.noreply.github.com>
2026-04-13 12:06:59 +0300

    solve task 2

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -1,1 +1,16 @@
 #!/usr/bin/env python3
+"""
+2. Measure the runtime
+"""
+from time import perf_counter
+from asyncio import run
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+def measure_time(n: int, max_delay: int) -> float:
+    '''
+    Return execution time for wait_n given `n` and `max_delay`.
+    '''
+    time_0 = perf_counter()
+    run(wait_n(n, max_delay))
+    return (perf_counter() - time_0) / n
