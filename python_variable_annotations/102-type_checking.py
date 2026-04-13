10cf14de98ae36e5a966367b6867812abd0f667e
m7md-d <236364775+m7md-d@users.noreply.github.com>
2026-04-13 08:06:41 +0300

    Solve task 102

---
+++ python_variable_annotations/102-type_checking.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+12. Type Checking
+"""
+from typing import Tuple, List
+
+
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    """
+    function zoom_array that takes a tuple lst and an int factor as arguments
+    and returns a List."""
+    zoomed_in: List = [
+        item for item in lst
+        for i in range(factor)
+    ]
+    return zoomed_in
+
+
+array = ([12, 72, 91], )
+
+zoom_2x = zoom_array(array)
+
+zoom_3x = zoom_array(array, int(3.0))
