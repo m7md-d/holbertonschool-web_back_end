9957136702b84bff3c30e111670570e188d5f259
m7md-d <236364775+m7md-d@users.noreply.github.com>
2026-04-13 12:45:03 +0300

    solve task 3

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -1,1 +1,13 @@
 #!/usr/bin/env python3
+"""
+3. Tasks
+"""
+from asyncio import create_task, Task
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int) -> Task:
+    '''
+    Returns asyncio.Task object
+    '''
+    return create_task(wait_random(max_delay))
