238fb4556e3b592b79da6283ecd5236d7a8b3995
m7md-d <236364775+m7md-d@users.noreply.github.com>
2026-04-13 13:43:02 +0300

    solve task 4

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -1,1 +1,19 @@
 #!/usr/bin/env python3
+"""
+4. Tasks
+"""
+from typing import List
+from asyncio import as_completed
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int = 10) -> List[float]:
+    '''Execute task_wait_random and returns sorted list of delay'''
+    tasks = [task_wait_random(max_delay) for ـ in range(n)]
+    delays = []
+
+    for completed_task in as_completed(tasks):
+        delay = await completed_task
+        delays.append(delay)
+
+    return delays
