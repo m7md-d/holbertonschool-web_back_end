8b4703e1e168dd3e3e6758ec7a9ace4c5165f830
m7md-d <236364775+m7md-d@users.noreply.github.com>
2026-04-13 17:03:23 +0300

    solve task 1

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,1 +1,14 @@
 #!/usr/bin/env python3
+"""
+1. Async Comprehensions
+"""
+from typing import List
+
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """
+    Return list of values yielded by async_generator.
+    """
+    return [value async for value in async_generator()]
