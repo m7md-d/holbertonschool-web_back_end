8563113cd0cdeba1b882132a220b92ebc4eb748d
m7md-d <236364775+m7md-d@users.noreply.github.com>
2026-05-09 22:48:15 +0300

    solve task 1: with the bad way

---
+++ ES6_classes/1-make_classrooms.js
@@ -0,0 +1,7 @@
+// Task 1: initializeRooms function
+
+import ClassRoom from './0-classroom.js';
+
+export default function initializeRooms() {
+  return [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)];
+}
