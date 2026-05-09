c1569496f4d10fada229063ff462584acfd9f470
m7md-d <236364775+m7md-d@users.noreply.github.com>
2026-05-09 23:25:35 +0300

    solve task 6

---
+++ ES6_classes/6-sky_high.js
@@ -0,0 +1,21 @@
+// Task 6: SkyHighBuilding class
+
+import Building from './5-building.js';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+
+    if (typeof floors !== 'number') {
+      throw new TypeError('Floors must be a number');
+    }
+    this._floors = floors;
+  }
+
+  get sqft() { return this._sqft; }
+  get floors() { return this._floors; }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors`;
+  }
+}
