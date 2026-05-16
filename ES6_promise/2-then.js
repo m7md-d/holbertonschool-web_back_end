6fe8d6a9c4066b052784ea122cf3f8301c63e9a3
m7md-d <236364775+m7md-d@users.noreply.github.com>
2026-05-16 21:58:15 +0300

    solve task 2

---
+++ ES6_promise/2-then.js
@@ -0,0 +1,13 @@
+// Task 2: handleResponseFromAPI function
+
+export default function handleResponseFromAPI(promise) {
+  return promise
+    .then(() => ({
+      status: 200,
+      body: 'success',
+    }))
+    .catch(() => new Error())
+    .finally(() => {
+      console.log('Got a response from the API');
+    });
+}
