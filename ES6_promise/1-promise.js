e28c207c50b20855bfac2864edba9455ec0769a7
m7md-d <236364775+m7md-d@users.noreply.github.com>
2026-05-16 21:49:54 +0300

    solve task 1

---
+++ ES6_promise/1-promise.js
@@ -0,0 +1,14 @@
+// Task 1: getFullResponseFromAPI function
+
+export default function getFullResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    if (success) {
+      resolve({
+        status: 200,
+        body: 'Success',
+      });
+    } else {
+      reject(new Error('The fake API is not working currently'));
+    }
+  });
+}
