f3ac4d2b83b6e5078453b92791f7b1010ce2ff69
m7md-d <236364775+m7md-d@users.noreply.github.com>
2026-05-16 22:03:43 +0300

    solve task 3

---
+++ ES6_promise/3-all.js
@@ -0,0 +1,15 @@
+// Task 3: handleProfileSignup function
+
+import { uploadPhoto, createUser } from "./utils";
+
+export default function handleProfileSignup() {
+  return Promise.all([uploadPhoto(), createUser()])
+    .then(([photoResponse, userResponse]) => {
+      console.log(
+        `${photoResponse.body} ${userResponse.firstName} ${userResponse.lastName}`,
+      );
+    })
+    .catch(() => {
+      console.log("Signup system offline");
+    });
+}
