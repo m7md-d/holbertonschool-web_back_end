9b4b29dd5a4c04d82c920282e34659ffcba5d6ba
m7md-d <236364775+m7md-d@users.noreply.github.com>
2026-05-16 14:16:30 +0300

    solve task 10

---
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,15 @@
+// Task 10: updateUniqueItems function
+
+export default function updateUniqueItems(map) {
+  if (!(map instanceof Map)) {
+    throw new Error('Cannot process');
+  }
+
+  for (const [key, value] of map.entries()) {
+    if (value === 1) {
+      map.set(key, 100);
+    }
+  }
+
+  return map;
+}
