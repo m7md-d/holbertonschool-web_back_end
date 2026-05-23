efd7226210cc90e0e8ef43c41fbc25a5e635c9d8
m7md-d <236364775+m7md-d@users.noreply.github.com>
2026-05-23 22:49:17 +0300

    --fix

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -3,38 +3,37 @@
 const fs = require('fs');
 
 const countStudents = (path) => {
-    try {
-        const data = fs.readFileSync(path);
-        const lines = data.toString().split('\n').filter((line) => line.trim() !== '');
+  try {
+    const data = fs.readFileSync(path);
+    const lines = data.toString().split('\n').filter((line) => line.trim() !== '');
 
-        if (lines.length <= 1) {
-            throw new Error('Cannot load the database');
-        }
-
-        const students = lines.slice(1).map((line) => {
-            return line.split(',');
-        });
-
-        const fieldMap = {};
-
-        for (const student of students) {
-            const [firstname, , , field] = student;
-            if (!fieldMap[field]) {
-                fieldMap[field] = [];
-            }
-            fieldMap[field].push(firstname);
-        }
-
-        console.log(`Number of students: ${students.length}`);
-
-        const fields = Object.keys(fieldMap).sort((a, b) => a.toLowerCase().localeCompare(b.toLowerCase()));
-
-        for (const field of fields) {
-            console.log(`Number of students in ${field}: ${fieldMap[field].length}. List: ${fieldMap[field].join(', ')}`);
-        }
-    } catch (error) {
-        throw new Error('Cannot load the database');
+    if (lines.length <= 1) {
+      throw new Error('Cannot load the database');
     }
+
+    const students = lines.slice(1).map((line) => line.split(','));
+
+    const fieldMap = {};
+
+    for (const student of students) {
+      const [firstname, , , field] = student;
+      if (!fieldMap[field]) {
+        fieldMap[field] = [];
+      }
+      fieldMap[field].push(firstname);
+    }
+
+    console.log(`Number of students: ${students.length}`);
+
+    const fields = Object.keys(fieldMap)
+      .sort((a, b) => a.toLowerCase().localeCompare(b.toLowerCase()));
+
+    for (const field of fields) {
+      console.log(`Number of students in ${field}: ${fieldMap[field].length}. List: ${fieldMap[field].join(', ')}`);
+    }
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
 };
 
 module.exports = countStudents;
