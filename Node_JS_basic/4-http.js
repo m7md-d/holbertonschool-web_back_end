feb2b5a06f866f0caf016a4a14d21a37d253ca56
m7md-d <236364775+m7md-d@users.noreply.github.com>
2026-05-23 23:06:34 +0300

    solve task 4

---
+++ Node_JS_basic/4-http.js
@@ -0,0 +1,13 @@
+// Task 4: Create a small HTTP server using Node's native http module
+
+const http = require('http');
+
+const app = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello Holberton School!');
+});
+
+app.listen(1245);
+
+module.exports = app;
